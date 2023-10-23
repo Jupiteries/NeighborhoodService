ca97b6a7db5523bd2062b751ed8a678ab1fd6636
Jupiteries <128291249+Jupiteries@users.noreply.github.com>
2023-10-23 14:25:52 +0400

    Create NeighborhoodInfo.cs

---
+++ NeighborhoodInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeighborhoodBuilder
+{
+    public class NeighborhoodInfo<T>
+    {
+        public T NumberOfHouses { get; set; }
+        public T ConditionOfHouses { get; set; }
+        public T ModernityOfHouses { get; set; }
+        public string isThereASchool { get; set; }
+        public T ConditionOfRoad { get; set; }
+        public T TransportAvailability { get; set; }
+        public T CrimeRates { get; set; }
+    }
+}
