4f339e147900802262aafe07ebb608f3ae67d8ee
Jupiteries <128291249+Jupiteries@users.noreply.github.com>
2023-10-23 14:26:34 +0400

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace NeighborhoodBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NeighborhoodInfo<int> neighborhood = new NeighborhoodInfo<int>();
+            CustomerInfo customer = new CustomerInfo();
+
+            try
+            {
+                Builder.CustomerInformation(customer);
+                Builder.Build(neighborhood, customer);
+            } catch
+            {
+                Console.WriteLine("You did something wrong.");
+            }
+        }
+    }
+}
